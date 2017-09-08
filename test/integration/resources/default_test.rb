24aa304fa993c09d777248fcd9c9c42e1003d8a3
Nick Gray <f0rkz@f0rkznet.net>
2017-09-07 22:21:42 -0400

    Fix the test

--- test/integration/resources/default_test.rb
+++ test/integration/resources/default_test.rb
@@ -10,13 +10,6 @@ describe file('/opt/steam/steamcmd.sh') do
   its('group') { should eq 'steam' }
 end
 
-describe file('/opt/arkserver/376030/ShooterGame') do
-  it { should exist }
-  it { should be_file }
-  it { should be_owned_by 'steam' }
-  its('group') { should eq 'steam' }
-end
-
 describe service('ark') do
   it { should be_installed }
   it { should be_enabled }
@@ -26,13 +19,12 @@ end
 ark_config = '/opt/arkserver/376030/ShooterGame/Saved/Config/LinuxNoEditor/GameUserSettings.ini'
 
 describe file(ark_config) do
+  it { should exist }
   it { should be_file }
+  it { should be_owned_by 'steam' }
+  its('group') { should eq 'steam' }
 end
 
 describe command('ps -ef | grep ShooterGameServer') do
   its('exit_status') { should eq 0 }
-end
-
-describe port(7777) do
-  it { should be_listening }
 end
